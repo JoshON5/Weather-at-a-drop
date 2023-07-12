9d8462214bc224d9d23b27fea951730da341f5b7
Joshua Nichols <joshua@DOOMBOT-2.local>
2023-07-11 21:03:04 -0500

    added a display for the date when the city is brought up

--- External_Code/script.js
+++ External_Code/script.js
@@ -21,6 +21,7 @@ var userValueEl = document.querySelector('#search-input');
 var searchButtonEl = document.querySelector('#search-btn');
 var currentCityEl = document.querySelector('#city');
 var displayCityEl = document.querySelector('#city-value');
+var displayDateEl = document.querySelector('#date');
 var weatherContainerEl = document.querySelector('#five-day-container');
 var userHistoryEl = document.querySelector('#searched-cities');
 var forecastCardsEl = document.querySelector('#forecast-cards');
@@ -53,15 +54,20 @@ var getWeather = function(lat, lon) {
     fetch(url).then(function (response) {
         if (response.ok) {
             response.json().then(function (data){
-                console.log(data.city.name);
+                console.log(data);
                 // displayWeather(data.list[0])
                 var cityName = data.city.name;
                 var tempVal = data.list[0].main.temp;
                 var windVal = data.list[0].wind.speed;
                 var humidityVal = data.list[0].main.humidity;
                 var icondId = data.list[0].weather[0].icon
+                var epochDate = data.list[0].dt
 
-                displayCityEl.innerHTML = cityName
+                var dateRaw = new Date(epochDate*1000)
+                var formattedDate = (dateRaw.getUTCMonth()) + '-' + dateRaw.getUTCDate() + '-' + dateRaw.getUTCFullYear()
+                
+                
+                displayCityEl.innerHTML = cityName + " ("+formattedDate+") "
 
                 var weatherImg = document.querySelector(".weather-icon");
                 weatherImg.innerHTML ="<img" + " src=" + "http://openweathermap.org/img/wn/"+icondId+"@2x.png>"
@@ -70,6 +76,8 @@ var getWeather = function(lat, lon) {
                 tempEl.innerHTML = tempVal + " °F"
                 windEl.innerHTML = windVal + " mph"
                 humidityEl.innerHTML = humidityVal + " %"
+
+                
             })
             
         }
