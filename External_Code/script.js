081e6cd5152287e7e8bce183ed1699487c2be26d
Joshua Nichols <joshua@DOOMBOT-2.local>
2023-07-11 19:37:34 -0500

    added the fetch for the weather display

--- External_Code/script.js
+++ External_Code/script.js
@@ -1,6 +1,4 @@
 var key = "6a6cbc32f4561808ace2b90501c16235";
-var city;
-var pastCity;
 // var errorMssg = (response) => {
 //     if (!response.ok) {
 //         throw Error(response.statusText)
@@ -26,6 +24,10 @@ var displayCityEl = document.querySelector('#city-value');
 var weatherContainerEl = document.querySelector('#five-day-container');
 var userHistoryEl = document.querySelector('#searched-cities');
 var forecastCardsEl = document.querySelector('#forecast-cards');
+var tempEl = document.querySelector('#temp');
+var windEl = document.querySelector('#wind');
+var humidityEl = document.querySelector('#humidity');
+var uvEl = document.querySelector('#uv-index');
 
 var userHistory = [];
 
@@ -41,8 +43,56 @@ function getCoordinates(city) {
     })
     .then (function (data){
         console.log(data.coord)
+        getWeather(data.coord.lat, data.coord.lon)
     })
+}
 
+
+var getWeather = function(lat, lon) {
+    var url = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=6a6cbc32f4561808ace2b90501c16235&units=imperial'
+    fetch(url).then(function (response) {
+        if (response.ok) {
+            response.json().then(function (data){
+                console.log(data.city.name);
+                // displayWeather(data.list[0])
+                var cityName = data.city.name;
+                var tempVal = data.list[0].main.temp;
+                var windVal = data.list[0].wind.speed;
+                var humidityVal = data.list[0].main.humidity;
+                var icondId = data.list[0].weather[0].icon
+
+                displayCityEl.innerHTML = cityName
+
+                var weatherImg = document.querySelector(".weather-icon");
+                weatherImg.innerHTML ="<img" + " src=" + "http://openweathermap.org/img/wn/"+icondId+"@2x.png>"
+                
+                
+                tempEl.innerHTML = tempVal + " °F"
+                windEl.innerHTML = windVal + " mph"
+                humidityEl.innerHTML = humidityVal + " %"
+            })
+            
+        }
+        // displayWeather();
+        // for (var i = 0; i < data.list[0].main.length; i++) {
+        //     console.log(main)
+        //     var temp = main[i].temp + "°F";
+        //     var humidity = main[i].humidity;
+        //     tempEl.innerHTML = temp;
+        //     humidityEl.innerHTML = humidity;
+        // }
+    })
+    // var displayWeather = function(weather, main) {
+        
+
+    //     // for (var i = 0; i < main.length; i++) {
+    //     //     console.log(main)
+    //     //     var temp = main[i].temp + "°F";
+    //     //     var humidity = main[i].humidity;
+    //     //     tempEl.innerHTML = temp;
+    //     //     humidityEl.innerHTML = humidity;
+    //     // }
+    // }
 }
 
 function formSubmitHandler (event) {
