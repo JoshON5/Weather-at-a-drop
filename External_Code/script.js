0c746dd500fdbe61d568df212612d924df2f4d70
Joshua Nichols <joshua@DOOMBOT-2.local>
2023-07-13 00:36:50 -0500

    added local storage functions and got them to append to the page as buttons that can be clicked to re run the search function

--- External_Code/script.js
+++ External_Code/script.js
@@ -86,80 +86,114 @@ var getWeather = function (lat, lon) {
 		}
 
 		function displayForecast(data) {
-			console.log(data.list[7].wind.speed)
+			console.log(data.list[7].wind.speed);
 			forecastCardsEl.innerHTML = "";
 
-			for (var i = 1; i < data.list.length; i+=8) {
-				var forecastCard = document.createElement("div")
-				forecastCard.setAttribute("class", "column mx-1 card has-background-info-dark")
-				var forecastCardTitle = document.createElement('p')
-				forecastCardTitle.setAttribute("class", "title has-text-white")
-				var forecastDate = document.createElement('p')
-				forecastDate.setAttribute("class", "has-text-white")
-				var forecastIcon = document.createElement('img')
-				var forecastTemp = document.createElement('p')
-				forecastTemp.setAttribute("class", "has-text-white")
-				var forecastWind = document.createElement('p')
-				forecastWind.setAttribute("class", "has-text-white")
-				var forecastHumidity = document.createElement('p')
-				forecastHumidity.setAttribute("class", "has-text-white")
+			for (var i = 1; i < data.list.length; i += 8) {
+				var forecastCard = document.createElement("div");
+				forecastCard.setAttribute(
+					"class",
+					"column mx-1 card has-background-info-dark"
+				);
+				var forecastCardTitle = document.createElement("p");
+				forecastCardTitle.setAttribute("class", "title has-text-white");
+				var forecastDate = document.createElement("p");
+				forecastDate.setAttribute("class", "has-text-white");
+				var forecastIcon = document.createElement("img");
+				var forecastTemp = document.createElement("p");
+				forecastTemp.setAttribute("class", "has-text-white");
+				var forecastWind = document.createElement("p");
+				forecastWind.setAttribute("class", "has-text-white");
+				var forecastHumidity = document.createElement("p");
+				forecastHumidity.setAttribute("class", "has-text-white");
 
-				var date = data.list[i].dt
-				var dateRaw = new Date(date * 1000)
-				var forecastDateEl = dateRaw.getUTCMonth() +
-				1 +
-				"-" +
-				dateRaw.getUTCDate() +
-				"-" +
-				dateRaw.getUTCFullYear()
+				var date = data.list[i].dt;
+				var dateRaw = new Date(date * 1000);
+				var forecastDateEl =
+					dateRaw.getUTCMonth() +
+					1 +
+					"-" +
+					dateRaw.getUTCDate() +
+					"-" +
+					dateRaw.getUTCFullYear();
 
-				var forecastIconUrl = data.list[i].weather[0].icon
+				var forecastIconUrl = data.list[i].weather[0].icon;
 
 				var temp = data.list[i].main.temp;
-				var tempRound = Math.round(temp)
+				var tempRound = Math.round(temp);
 
 				forecastCardTitle.innerHTML = data.city.name;
 				forecastDate.textContent = forecastDateEl;
-				forecastIcon.setAttribute("src", "http://openweathermap.org/img/wn/"+forecastIconUrl+"@2x.png");
+				forecastIcon.setAttribute(
+					"src",
+					"http://openweathermap.org/img/wn/" + forecastIconUrl + "@2x.png"
+				);
 				forecastTemp.textContent = "Temp: " + tempRound + " \xB0" + "F";
 				forecastWind.textContent = "Wind: " + data.list[i].wind.speed + " mph";
-				forecastHumidity.textContent = "Humidity: " + data.list[i].main.humidity + " %";
+				forecastHumidity.textContent =
+					"Humidity: " + data.list[i].main.humidity + " %";
 
-				forecastCard.append(forecastCardTitle, forecastDate, forecastIcon, forecastTemp, forecastWind, forecastHumidity)
+				forecastCard.append(
+					forecastCardTitle,
+					forecastDate,
+					forecastIcon,
+					forecastTemp,
+					forecastWind,
+					forecastHumidity
+				);
 				forecastCardsEl.append(forecastCard);
 			}
-
-
-
 		}
-
 	});
-
-
 };
 
-function setSearchHistory() {
 
-	var searchHistory = JSON.parse(localStorage.getItem("searchHistory")) || [];
-	console.log(currentCityEl.value);
-	console.log(searchHistory);
-	if (!searchHistory.includes(currentCityEl.value)) {
-		searchHistory.push(currentCityEl.value)
-		localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
-
+function displayHistory() {
+	userHistoryEl.innerHTML = "";
+	for (var i = 0; i <= userHistory.length - 1; i++) {
+		var searchButtonEl = document.createElement("button");
+		searchButtonEl.setAttribute("type", "button");
+		searchButtonEl.classList = "button is-info is-fullwidth";
+		searchButtonEl.setAttribute("data-search", userHistory[i]);
+		searchButtonEl.textContent = userHistory[i];
+		userHistoryEl.append(searchButtonEl);
 	}
-	
-	
 }
 
+function setHistory(info) {
 
+	if (!userHistory.includes(currentCityEl.value)) {
+	userHistory.push(info);
+	localStorage.setItem("id", JSON.stringify(userHistory));
+	}
+	displayHistory();
+}
+
+function getHistory() {
+	var storageHistory = localStorage.getItem("id");
+	if (storageHistory) {
+		userHistory = JSON.parse(storageHistory);
+	}
+	displayHistory();
+}
 
 function formSubmitHandler(event) {
 	event.preventDefault();
 	var search = currentCityEl.value.trim();
 	getCoordinates(search);
-	setSearchHistory(search);
+	setHistory(search);
 }
 
+function historyHandler (event) {
+	if (!event.target.matches("button")) {
+	  return;
+	}
+	var buttonClick = event.target;
+	var search = buttonClick.getAttribute("data-search");
+	getCoordinates(search);
+  };
 
+getHistory();
+
+userHistoryEl.addEventListener("click", historyHandler);
 userValueEl.addEventListener("submit", formSubmitHandler);
